102046b0c1583923e48d38c00860561cc46ed9b5
HuangMing1203 <61956773+HuangMing1203@users.noreply.github.com>
2026-03-11 14:34:49 +0000

    refactor: update App component layout with Material-UI for improved structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,52 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+import AppBar from '@mui/material/AppBar'
+import Card from '@mui/material/Card'
+import Container from '@mui/material/Container'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Vite + React
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container
+        maxWidth="xl"
+        sx={{ my: 4, display: 'flex', flexFlow: 'nowrap column', gap: 3 }}
+      >
+        <Card sx={{ maxWidth: 1280, margin: '0 auto', padding: '2rem', textAlign: 'center' }}>
+          <div>
+            <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </Card>
+      </Container>
     </>
   )
 }
-
-export default App
